c22deecbf40fc58c12b44d6ec69b83f59382ee4a
AsanoYuta <119588850+AsanoYuta@users.noreply.github.com>
2023-02-20 10:53:40 +0000

    migrate

--- db/migrate/20230206180435_create_movies.rb
+++ db/migrate/20230206180435_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title, null: false, default: ""
-      t.text :body, null: false, default: ""
+      t.text :body
       t.float :comfort, null: false, default: 0
       t.integer :user_id
       t.integer :genre_id
