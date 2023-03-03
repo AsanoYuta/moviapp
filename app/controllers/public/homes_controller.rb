80b605bbdd725cfbadd61f41cc1bf10ae276204c
AsanoYuta <119588850+AsanoYuta@users.noreply.github.com>
2023-03-03 16:01:12 +0000

    f

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,8 +2,9 @@ class Public::HomesController < ApplicationController
   require 'themoviedb-api'
   Tmdb::Api.key("2f65e50b9800d29491f553df62f77f95")
   Tmdb::Api.language("ja")
-  
+
   def top
+    @movies = Movie.page(params[:page]).per(100)
   end
 
   def about
